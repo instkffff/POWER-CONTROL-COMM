6556397f16971b7600d2d07f8579dca1604d2362
NightCandle <instkffff@gmail.com>
2025-09-10 00:15:56 +0800

    .

---
+++ startup.ps1
@@ -0,0 +1,15 @@
+Write-Host "starting..." -ForegroundColor Yellow
+
+# 启动 index.js
+Write-Host "start index.js..." -ForegroundColor Cyan
+pm2 start index.js
+
+# 启动 server.js
+Write-Host "start server.js..." -ForegroundColor Cyan
+pm2 start server.js
+
+# 显示当前运行的应用列表
+Write-Host "list:" -ForegroundColor Yellow
+pm2 list
+
+Write-Host "finish" -ForegroundColor Green
