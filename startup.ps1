26f643b436a90e25b410150f82658e3a491af870
NightCandle <instkffff@gmail.com>
2025-09-30 15:15:47 +0800

    .

--- startup.ps1
+++ startup.ps1
@@ -2,11 +2,11 @@ Write-Host "starting..." -ForegroundColor Yellow
 
 # 启动 index.js
 Write-Host "start index.js..." -ForegroundColor Cyan
-pm2 start index.js
+pm2 start index.js --no-monitor
 
 # 启动 server.js
 Write-Host "start server.js..." -ForegroundColor Cyan
-pm2 start server.js
+pm2 start server.js --no-monitor
 
 Write-Host "pm2-logrotate setting..." -ForegroundColor Cyan
 pm2 set pm2-logrotate:retain 4
